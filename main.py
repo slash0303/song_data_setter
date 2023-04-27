8cb5ae9cb4265c3a41e8867bc70b4b7729132c58
slash0303 <59768035+slash0303@users.noreply.github.com>
2023-04-27 23:54:29 +0900

    add mode input

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import eyed3
+import os
 from eaxtension import LogE
 
-name = input("Enter song name: ")
+# command text alias
+command_alias = {"exit" : 1 }
 
-if name.find(".")==-1:
-    ext = input("extension cannot found. please enter the extension: ")
-    if ext.find(".")==-1:
-        name += ("." + ext)
-    else:
-        name += ext
-    LogE.g("modify", f"file name is modified to '{name}'")
-    
-LogE.g("song name", name)
+def change_meta():
+    name = input("Enter song name: ")
 
-song = eyed3.load(name)
+    if name[0] == "/":
+    elif name.find(".")==-1:
+        ext = input("extension cannot found. please enter the extension: ")
+        if ext.find(".")==-1:
+            name += ("." + ext)
+        else:
+            name += ext
+        LogE.g("modify", f"file name is modified to '{name}'")
+
+    LogE.g("song name", name)
+
+    song = eyed3.load(name)
+
+mode = input("What do you want?[change metadata/ls]: ")
+
+if "cmd".find(mode) or "change metadata".find(mode):
+    change_meta()
