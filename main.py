3aa0e78782647c31b02659d982be287346b41877
slash0303 <59768035+slash0303@users.noreply.github.com>
2023-04-29 12:28:49 +0900

    add tag edit code

--- main.py
+++ main.py
@@ -2,43 +2,70 @@ import eyed3
 import os
 from eaxtension import LogE
 
-# command text alias
-command_alias = {"exit" : 1 }
+# command mode: - fucntion alias
+command_alias = {"ls": "" }
 
-# metadata change manual
+# metadata: change manual
 meta_man = ["song name",
             "artist name",
             "album",
-            "album art",
+            "album artist",
             "lyrics (txt file location)",]
 
 # metadata change function
 def change_meta():
-    name = input("Enter song name: ")
+    while True:
+        try:
+            name = input("Enter file name: ")
 
-    # command
-    if name[0] == "/":
-        pass
-        # TODO : make command feature
-    # intergrity check
-    elif name.find(".")==-1:
-        ext = input("extension cannot found. please enter the extension: ")
-        if ext.find(".")==-1:
-            name += ("." + ext)
-        else:
-            name += ext
-        LogE.g("modify", f"file name is modified to '{name}'")
+            # command
+            if name[0] == "/":
+                pass
+                # TODO : make command feature
+            # file name intergrity check
+            elif name.find(".mp3")==-1 or name.find(".wav")==-1 or name.find(".flac")==-1:
+                ext = input("extension cannot found. please enter the extension: ")
+                if ext.find(".")==-1:
+                    name += ("." + ext)
+                else:
+                    name += ext
+
+                # music load
+                song = eyed3.load(name).tag
+                break
+        except:
+            print("file name is incorrect. please re-write file name.")
+
+    LogE.g("modify", f"file name is modified to '{name}'")
 
     LogE.g("music file name", name)
 
-    # music load
-    song = eyed3.load(name)
 
+
+    # metadata: mode index - function mapping
+    meta_alias = {0: song.title,
+                  1: song.artist,
+                  2: song.album,
+                  3: song.album_artist,
+                  4: song.lyrics}
+
+    # change meta data
     for i, x in enumerate(meta_man):
         a = input("Enter '" + x + "' (pass key: /!/): ")
+        meta_alias[i] = a
 
+    for x in meta_alias.values():
+        print(x)
 
-mode = input("What do you want?[change metadata/ls]: ")
+    save_yn = input("is it correct?[Y/n]: ")
+    if save_yn == "Y" or save_yn == "y":
+        song.save()
 
-if "cmd".find(mode) or "change metadata".find(mode):
+mode = input("What do you want?[change metadata/command mode]: ")
+
+if "change metadata".find(mode) != -1:
     change_meta()
+
+elif "command mode".find(mode) != -1:
+    pass
+    # TODO : make command feature
