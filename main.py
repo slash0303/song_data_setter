d3f4438f990f1035d238711e133a93190970600e
slash0303 <59768035+slash0303@users.noreply.github.com>
2023-04-29 10:22:53 +0900

    remove duplicate file

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ from eaxtension import LogE
 # command text alias
 command_alias = {"exit" : 1 }
 
+# metadata change manual
+meta_man = ["song name",
+            "artist name",
+            "album",
+            "album art",
+            "lyrics (txt file location)",]
+
+# metadata change function
 def change_meta():
     name = input("Enter song name: ")
 
+    # command
     if name[0] == "/":
+        pass
+        # TODO : make command feature
+    # intergrity check
     elif name.find(".")==-1:
         ext = input("extension cannot found. please enter the extension: ")
         if ext.find(".")==-1:
@@ -17,9 +29,14 @@ def change_meta():
             name += ext
         LogE.g("modify", f"file name is modified to '{name}'")
 
-    LogE.g("song name", name)
+    LogE.g("music file name", name)
 
+    # music load
     song = eyed3.load(name)
+
+    for i, x in enumerate(meta_man):
+        a = input("Enter '" + x + "' (pass key: /!/): ")
+
 
 mode = input("What do you want?[change metadata/ls]: ")
 
