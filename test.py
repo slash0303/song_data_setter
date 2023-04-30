cb560e4cc9eedb89439f2fb834669fc42054ba38
slash0303 <59768035+slash0303@users.noreply.github.com>
2023-04-30 12:46:08 +0900

    edit test file to change song title

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ song = eyed3.load("Egzod - Rise Up (ft. Veronica Bravo & M.I.M.E) [NCS Release].
 
 song = song.tag
 
-song.artist = "hello"
+song.title = "hello"
 
 song.save(version=eyed3.id3.ID3_V2_3)
