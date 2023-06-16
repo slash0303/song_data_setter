9492b02672680cc0d0cfbc0a65d4e527a3f03262
slash0303 <59768035+slash0303@users.noreply.github.com>
2023-06-16 22:18:28 +0900

    fix extension detection code

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ meta_man = ["title of song",
             "name of album artist",
             "lyrics (only path without file name)",]
 
+# extension list
 ext_list = [".mp3", ".wav", ".m4a", ".mpeg"]
-ext_in = False
 
 # metadata change function (manually)
 def change_meta_manual():
     while True:
+        ext_in_name = False
         try:
             name = input("Enter target file name: ")
 
@@ -27,14 +28,16 @@ def change_meta_manual():
                 # TODO make command feature
             # file name intergrity check
             else:
+                name = name.split(".")
+                name = name[0]
                 for x in ext_list:
-                    if name.find(x) < 0:
-                        ext_in = True
-                if ext_in:
+                    if name.find(x) > 0:
+                        ext_in_name = True
+                if ext_in_name:
                     pass
                 else:
                     ext = input("extension cannot found. please enter the extension: ")
-                    if ext.find(".")<0:
+                    if ext.find(".") < 0:
                         name += ("." + ext)
                     else:
                         name += ext
@@ -54,10 +57,7 @@ def change_meta_manual():
 
     # change meta data
     for x in meta_man:
-        data_input = input("Enter '" + x + "' (pass key: /!/): ")
-        if data_input == "/!/":
-            input_list[x] = ""
-            continue
+        data_input = input("Enter '" + x + "': ")
         if x == "title of song":
             song.title = data_input
             input_list[x] = data_input
@@ -70,19 +70,22 @@ def change_meta_manual():
         elif x == "name of album artist":
             song.album_artist = data_input
             input_list[x] = data_input
-        elif x.find("lyrics") != -1:
+        elif x.find("lyrics") < 0:
             lyr_dir = data_input
             try:
-                os.chdir(lyr_dir)
+                if lyr_dir == "" or lyr_dir == ".":
+                    lyr_dir = "current directory"
+                else:
+                    os.chdir(lyr_dir)
                 lyr_name = input("Enter file name: ")
                 with open(lyr_name, "r", encoding="utf-8") as lyr:
                     lyr = lyr.readlines()
                     lyr = "".join(lyr)
                     song.lyrics.set(lyr)
-                input_list[x] = data_input
+                input_list[x] = lyr_dir
             except Exception as e:
                 LogE.e("", e)
-                input_list[x] = ""
+                input_list[x] = "missing"
 
     # re-check input value before saving data
     for x in input_list.keys():
@@ -105,13 +108,13 @@ def change_meta_auto():
 mode = input("What do you want?[change metadata/command mode]: ")
 if "change metadata".find(mode) != -1:
     # select manual or automatic
-    mode = input("manual / autelvmfomatic: ")
+    mode = input("manual / automatic: ")
     if "manual".find(mode) != -1:
         change_meta_manual()
     elif "automatic".find(mode) != -1:
         # input path and intergrity test
         while True:
-            path = input("please enter working path(default is pwd): ")
+            path = input("please enter working path(default path is current dir): ")
             if path == "":
                 pass
             else:
